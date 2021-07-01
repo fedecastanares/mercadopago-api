cf106248a02fd7246623e55112fd2ac5ae9d3f62
Federico <federico.castanares@gmail.com>
2021-07-01 18:56:35 -0300

    notificacion

--- controllers/notificaciones.js
+++ controllers/notificaciones.js
@@ -1,3 +1,5 @@
 module.exports = async (request, response) => { 
+    console.log('*********** NOTIFICACION **************');
     console.log(request.body);
+    console.log('*********** NOTIFICACION **************');
 }
