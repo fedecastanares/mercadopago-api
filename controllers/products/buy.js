29d14c23b3e3739531f677b2e367419881912d99
Federico <federico.castanares@gmail.com>
2021-07-02 13:26:39 -0300

    leyendo hardcode

--- controllers/products/buy.js
+++ controllers/products/buy.js
@@ -14,7 +14,7 @@ module.exports = async (request, response) => {
       email: "test_user_63274575@testuser.com",
       phone: {
         area_code: "11",
-        number: "22223333"
+        number: 22223333
       },
       address: {
         zip_code: "1111",
@@ -22,16 +22,7 @@ module.exports = async (request, response) => {
         street_number: 123
       }
     },
-    items: {
-      title: product.title,
-      description: "Dispositivo móvil de Tienda e-commerce",
-      picture_url: product.thumbnail,
-      // category_id: product.category_id,
-      unit_price: product.price,
-      //currency_id: product.currency_id,
-      quantity: 1,
-      id: 1234
-    },
+    items: request.body.items,
     back_urls: {
       success: `${process.env.URL_FRONT}/success`,
       pending: `${process.env.URL_FRONT}/pending`,
